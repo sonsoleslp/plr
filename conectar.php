27f9d60cbe1cea4c0a5ce5ba1bb904aca6dbd9a2
sonsoleslp <sonsoleslp@hotmail.com>
2016-05-10 23:26:07 +0200

    .gitignore is now working

--- conectar.php
+++ conectar.php
@@ -1,9 +1,6 @@
 <?php
 
-	$dbhost = "mysql6.000webhost.com";
-	$dbuser = "a5462179_plr";
-	$dbpass = "9712645a";
-	$dbname = "a5462179_plr";
+
 	try {
 		$db = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass,  
 				     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
